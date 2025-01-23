842089ed3d439fb7c2c6c462eef9b7899c975c12
Oluwasanmi Ariyo <52462519+vic1157@users.noreply.github.com>
2025-01-23 17:37:56 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section className="py-24">
+		<div className="container">
+			<h1 className="text-3xl font-bold"> Next TS Starter</h1>
+		</div>
+	</section>
+  );
+}
